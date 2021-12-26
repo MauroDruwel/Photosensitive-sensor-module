0b7c6bf0eba535497cbb58967beda1948fd6d63b
codermauro <mauro.druwel@gmail.com>
2021-12-26 19:23:39 +0100

    Add examples

--- examples/WPSE352_example.ino
+++ examples/WPSE352_example.ino
@@ -1,15 +1,17 @@
 /*
-  Analog input, serial output
-  Reads an analog input pin, prints the result to the Serial Monitor.
-  created 29 Dec. 2008
-  modified 9 Apr 2012
-  by Tom Igoe
-  modified 8 March 2021
-  by Midas Gossye (Whadda/Velleman)
-  Based on Arduino AnalogInOutSerial code
-  This example code is in the public domain.
-  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
-*/
+  @@@@@@@@@@@@@@@@@@@@@@                                                                                                                               
+  @@@@@@@@@@@@@@@@@@@@@@             @@@.    @@@    @@@.     @@@@    @@@@        @@@@@@@@@        @@@@@@@@@@@@@.     .@@@@@@@@@@@@@        @@@@@@@@@  
+  @@@@@%%@@@%%@@@%%@@@@@             @@@@    @@@    @@@@     @@@@    @@@@       @@@@@@@@@@        @@@@@@@@@@@@@@     @@@@@@@@@@@@@@        @@@@@@@@@  
+  @@@@@  @@@  @@@  @@@@@             @@@@    @@@    @@@@     @@@@    @@@@       @@@@   @@@@        @@@@@   @@@@@       @@@@@   @@@@       @@@@   @@@@ 
+  @@@@@            @@@@@             @@@@   @@@@    @@@@     @@@@@@@@@@@@       @@@@   @@@@        @@@@@   @@@@@       @@@@@   @@@@       @@@@   @@@@ 
+  @@@@@   @    @   @@@@@             @@@@   @@@@    @@@@     @@@@@@@@@@@@       @@@@@@@@@@@        @@@@@   @@@@@       @@@@@   @@@@       @@@@@@@@@@@ 
+  @@@@@            @@@@@             @@@@###@@@@@##@@@@@     @@@@    @@@@      @@@@@@@@@@@@       #@@@@@###@@@@@     ##@@@@@###@@@@       @@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@@@@@@             @@@@@@@@@@@@@@@@@@@     @@@@    @@@@      @@@@    @@@@@      @@@@@@@@@@@@@@     @@@@@@@@@@@@@@      @@@@     @@@@
+  @@@@@@@@@@@@@@@@@@@@@@
+  
+  Whadda PHOTOSENSITIVE SENSOR MODULE
+  
+ */
 
 // These constants won't change. They're used to give names to the pins used:
 const int analogInPin = A0;  // Analog input pin that the light sensor is attached to
