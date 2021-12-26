384ed262653f7830d67947d9e32f0ba62d380691
codermauro <mauro.druwel@gmail.com>
2021-12-26 19:28:10 +0100

    Add examples

--- examples/WPSE352_example2.ino
+++ examples/WPSE352_example2.ino
@@ -14,31 +14,23 @@
  */
 
 // These constants won't change. They're used to give names to the pins used:
-const int digitalInPin = 2;  // Analog input pin that the light sensor is attached to
-const int ledPin = 13; // Digital output pin that the led is attached to
+const int analogInPin = A0;  // Analog input pin that the light sensor is attached to
 
-bool sensorValue;        // value read from the light sensor
+int sensorValue = 0;        // value read from the light sensor
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // read the analog in value:
-  sensorValue = digitalRead(digitalInPin);
+  sensorValue = analogRead(analogInPin);
 
   // print the results to the Serial Monitor:
   Serial.print("sensor = ");
   Serial.println(sensorValue);
-  // turn the led on or off
-  if(sensorValue){
-    digitalWrite(ledPin, HIGH);
-  }
-  else{
-    digitalWrite(ledPin, LOW);
-  }
+
   // wait 10 milliseconds before the next loop for the analog-to-digital
   // converter to settle after the last reading:
   delay(10);
